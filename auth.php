84e9c2530a19b0ac9da3fdc7a4c97e66bca29204
aryansenpaii <aryansenpai256@gmail.com>
2024-01-18 15:04:35 +0530

    added session functionalities

--- auth.php
+++ auth.php
@@ -1,16 +1,16 @@
-<?php 
 
+<?php 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
     $sql = "SELECT * FROM employee_login WHERE username = '$username' AND password = '$password'";
     $result = mysqli_query($conn, $sql);
-    // $resultArray = mysqli_fetch_assoc($result);
-    // $access_type = $resultArray['access_type'];
+    $resultArray = mysqli_fetch_assoc($result);
+    $access_type = $resultArray['access_type'];
 
     if ($result && mysqli_num_rows($result) > 0) {
-        // $_SESSION['user_role'] = $access_type; // or 'salesman'
+        $_SESSION['user_role'] = $access_type; // or 'salesman'
         header("Location: dashboard.php");
                 exit();
     } else {
