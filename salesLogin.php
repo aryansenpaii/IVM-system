d5748de52fc511fb004ae12ba1741d8cc1e50391
patwalbhumika <bhumikapatwal19@gmail.com>
2024-01-18 16:13:46 +0530

    files added successfully

--- salesLogin.php
+++ salesLogin.php
@@ -5,12 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IVM-GEU</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
-
-
-
     <div class=container>
         <h1>Welcome SalesPerson</h1>
         <h3>Please enter your details</h3>
@@ -18,19 +16,13 @@
             <input type="text" name="username" id="username" placeholder="Enter your username">
             <input type="password" name="password" id="password" placeholder="Enter your password">
             <button type="submit" class="loginButton">Log In</button>
+            <a  class="loginButton" href="index.php">Back</a>
         </form>
-
         <?php
-        include('db_connection.php');
-        include('auth.php');
-        $_SESSION['user_role'] = 'sales'; // or 'salesman'
+            include('db_connection.php');
+            include('auth.php');
+            $_SESSION['user_role'] = 'sales'; // or 'salesman'
         ?>
-
-<a href="index.php">Back</a>
-
     </div>
-
-
 </body>
-
 </html>
